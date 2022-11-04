9c0dcb93eb5f611c37b9b5e8f1ac0445ad05470e
jpalomera <jpalomera@gmail.com>
2022-11-04 13:09:33 -0300

    Add files via upload

---
+++ logica.php
@@ -0,0 +1,13 @@
+<?php
+# Ejemplo en PHP
+# ----------------
+# Declaración de variables:
+$gatos_felinos = true;
+$gatos_tienen_4_patas = true;
+
+var_dump( $gatos_felinos and $gatos_tienen_4_patas ); # Se imprimirá en consola/navegador el tipo de dato y valor de la evaluación.
+
+echo "\n"; # Salto de línea.
+# ----------------
+# Impresión en consola/navegador:
+# bool(true)
